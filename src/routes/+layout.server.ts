122c454162b08312ad32c235c1b49abf8282c265
Sebastiano Racca <sebastiano@racca.me>
2026-02-16 23:13:15 +0100

    .

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,9 @@
 import { locales } from '$lib/i18n';
 
+export const prerender = true;
+export const ssr = true;
+export const csr = true;
+
 export const load = () => {
 	return {
 		locale: locales.it,
