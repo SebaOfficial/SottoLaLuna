a9df160d32ae621478ef3aa65dfefb9581a27afb
Sebastiano Racca <sebastiano@racca.me>
2026-08-11 18:10:08 +0200

    fix: add menu to sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,4 +1,5 @@
 import config from '$lib/config';
+import menu from '$lib/assets/menu.pdf?url';
 
 export const prerender = true;
 
@@ -21,9 +22,15 @@ export const GET = async () =>
 	</url>
 
 	<url>
+		<loc>${config.baseUrl}${menu}</loc>
+		<lastmod>${new Date().toISOString().slice(0, 10)}</lastmod>
+		<priority>0.8</priority>
+	</url>
+	
+	<url>
 		<loc>${config.baseUrl}/privacy-policy</loc>
 		<lastmod>2026-03-13</lastmod>
-		<priority>0.8</priority>
+		<priority>0.5</priority>
 	</url>
 </urlset>
 `.trim(),
